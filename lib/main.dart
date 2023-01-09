a13e701c6c42b6ec3fa42b1ddfa84cba49dbfd9b
HP <rajkhatri23066@gmail.com>
2023-01-09 10:59:44 +0530

    9-1-23

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:untitled3/homePage.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ),
+  );
+}
